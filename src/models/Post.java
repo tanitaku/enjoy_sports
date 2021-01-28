c9884076592c288cd060d79b6e6ae80067783dd8
takuma.t <takumatanioka1111@gmail.com>
2021-01-28 16:30:17 +0900

    Add Post class

---
+++ src/models/Post.java
@@ -0,0 +1,132 @@
+package models;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "posts")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllPosts",
+        query = "SELECT p FROM Post AS p ORDER BY p.id DESC"
+    ),
+    @NamedQuery(
+        name = "getItemsCount",
+        query = "SELECT COUNT(p) FROM Post AS p"
+    ),
+    @NamedQuery(
+        name = "getMyAllItems",
+        query = "SELECT p FROM Post AS p WHERE p.user = :user ORDER BY p.id DESC"
+        ),
+    @NamedQuery(
+        name = "getMyItemsCount",
+        query = "SELECT COUNT(p) FROM Post AS p WHERE p.user = :user"
+        ),
+
+})
+
+@Entity
+public class Post {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "post_date", nullable = false)
+    private Date post_date;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "sport", nullable = false)
+    private String sport;
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getPost_date() {
+        return post_date;
+    }
+
+    public void setPost_date(Date post_date) {
+        this.post_date = post_date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
